19ef351012120e8bd5ae9146203babd777a4910a
Ruslan <feduslan@gmail.com>
2024-07-03 17:17:06 +0300

    Final tasks_app

--- repositories.py
+++ repositories.py
@@ -1,24 +1,28 @@
 from sqlalchemy import select
 from database import session, TaskOrm
-from scheamas import STaskAdd
+from scheamas import STask, STaskAdd
 
 class TaskRepository:
     @classmethod
     async def add_one(cls, data:STaskAdd):
-        async with session() as session:
+        async with session() as s:
             task_dict = data.model_dump()
 
             task = TaskOrm(**task_dict)
-
-            session.add(task)
-            await session.flush()
-            await session.commit()
+            s.add(task)
+            await s.flush()
+            await s.commit()
             return task.id
-        
+
     @classmethod
-    async def find_all(cls):
-        async with session() as session:
+    async def find_all(cls) -> list[STask]:
+        async with session() as s:
             query = select(TaskOrm)
-            result = await session.execute(query)
-            task_model = result.scalars().all()
-            return task_model
+            result = await s.execute(query)
+            task_models = result.scalars().all()
+            task_dict = [orm_to_dict(task_model) for task_model in task_models]
+            task_schemas = [STask.model_validate(task) for task in task_dict]
+            return task_schemas
+        
+def orm_to_dict(orm_model):
+    return {column.name: getattr(orm_model, column.name) for column in orm_model.__table__.columns}
--- router.py
+++ router.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter
 from repositories import TaskRepository
-from scheamas import STaskAdd
+from scheamas import STaskAdd, STask, STaskId
 
 router = APIRouter(prefix="/tasks", tags=["Тасочки"])
 
 
 
 @router.post("")
-async def add_task(task:STaskAdd):
+async def add_task(task:STaskAdd) -> STaskId:
     task_id = await TaskRepository.add_one(task)
     return {"ok":True, "task_id":task_id}
 
 @router.get("")
-async def get_home():
+async def get_task() -> list[STask]:
     tasks = await TaskRepository.find_all()
-    return {"name":tasks}
+    return tasks
